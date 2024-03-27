43a8b8f0f868cfd52f29d01da9ddf03ea8f66c5a
phyastro <akshithrama2008@gmail.com>
2024-03-27 08:56:21 +0530

    Create Framebuffers

--- src/vulkanapp.cpp
+++ src/vulkanapp.cpp
@@ -132,6 +132,7 @@ private:
 	VkRenderPass renderPass;
 	VkPipelineLayout pipelineLayout;
 	VkPipeline graphicsPipeline;
+	std::vector<VkFramebuffer> swapChainFramebuffers;
 
     void InitWindow() {
         SDL_Init(SDL_INIT_VIDEO);
@@ -700,6 +701,28 @@ private:
 		vkDestroyShaderModule(device, vertexShaderModule, nullptr);
 	}
 
+	void CreateFramebuffers() {
+		swapChainFramebuffers.resize(swapChainImageViews.size());
+		for (size_t i = 0; i < swapChainImageViews.size(); i++) {
+			VkImageView attachments[] = {
+				swapChainImageViews[i]
+			};
+
+			VkFramebufferCreateInfo framebufferInfo{};
+			framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_ATTACHMENTS_CREATE_INFO;
+			framebufferInfo.renderPass = renderPass;
+			framebufferInfo.attachmentCount = 1;
+			framebufferInfo.pAttachments = attachments;
+			framebufferInfo.width = swapChainExtent.width;
+			framebufferInfo.height = swapChainExtent.height;
+			framebufferInfo.layers = 1;
+
+			if (vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS) {
+				throw std::runtime_error("Failed To Create Framebuffer!");
+			}
+		}
+	}
+
     void InitVulkan() {
         CreateInstance();
 		SetupDebugMessenger();
@@ -710,6 +733,7 @@ private:
 		CreateImageViews();
 		CreateRenderPass();
 		CreateGraphicsPipeline();
+		CreateFramebuffers();
     }
     
     void MainLoop() {
@@ -721,6 +745,9 @@ private:
     }
 
     void CleanUp() {
+		for (VkFramebuffer framebuffer : swapChainFramebuffers) {
+			vkDestroyFramebuffer(device, framebuffer, nullptr);
+		}
 		vkDestroyPipeline(device, graphicsPipeline, nullptr);
 		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
 		vkDestroyRenderPass(device, renderPass, nullptr);
