82210950ae6e1471ee43e6b15fedb59d788617ea
phyastro <akshithrama2008@gmail.com>
2024-10-14 14:16:18 +0530

    Added SmoothCuboid, DupinCyclide
    And Some Tiny Changes

---
+++ cmake_clean.bat
@@ -0,0 +1,1 @@
+C:\Strawberry\c\bin\cmake.EXE --build "c:/Users/adars/Documents/Visual Studio Code/C++/PathTracer/build" --config Release --target clean
---
+++ cmake_init.bat
@@ -0,0 +1,4 @@
+C:\Strawberry\c\bin\cmake.EXE --version
+C:\Strawberry\c\bin\cmake.EXE -E capabilities
+C:\Strawberry\c\bin\gcc.exe -v
+C:\Strawberry\c\bin\cmake.EXE -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=C:\Strawberry\c\bin\gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:\Strawberry\c\bin\g++.exe --no-warn-unused-cli "-SC:/Users/adars/Documents/Visual Studio Code/C++/PathTracer" "-Bc:/Users/adars/Documents/Visual Studio Code/C++/PathTracer/build" -G Ninja
