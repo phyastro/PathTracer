60d1703e9f138b5b8f166a26d03028520844544f
phyastro <akshithrama2008@gmail.com>
2025-02-20 15:24:56 +0530

    Added Faster Version For Mandelbulb
    Using Chebynskev Polynomials

--- sdfs/mandelbulb.glsl
+++ sdfs/mandelbulb.glsl
@@ -11,28 +11,34 @@ float sdf(in vec3 p)
         // Scalar Derivative
         dr = 8.0 * pow(r, 7.0) * dr + 1.0;
 
+        float invr = 1.0 / r;
+        float invl = inversesqrt(dot(vec2(z.x, z.z), vec2(z.x, z.z)));
+        r *= r;
+        r *= r;
+        r *= r;
+
         // f_n(c) = f_n-1(c)^8 + c
-        // Something Is Different With The Structure When We Go From 2 To 4 To 8 Times The Angle Than Directly Going To 8 Times The Angle.
-        /*
-        for (int k = 0; k < 3; k++) {
-            float tant2 = z.x / z.z;
-            float cosp2 = z.y / r;
-            float cosp = 2.0 * cosp2 * cosp2 - 1.0;
-            float sinp = 2.0 * sqrt(1.0 - cosp2 * cosp2) * cosp2;
-            float cost = (2.0 / (tant2 * tant2 + 1.0)) - 1.0;
-            float sint = 2.0 * tant2 / (tant2 * tant2 + 1.0);
-            r *= r;
-            z = r * vec3(sint * sinp, cosp, cost * sinp);
-        }
+        // Using Chebynshev Polynomials
+        // Faster Than Inverse Trigonometric Functions
+        float cost = z.z * invl;
+        float cosp = z.y * invr;
+        float sint = z.x * invl;
+        float sinp = invr / invl;
+        float cost2 = cost * cost;
+        float cosp2 = cosp * cosp;
+        float cost8 = fma(fma(fma(fma(128.0, cost2, -256.0), cost2, 160.0), cost2, -32.0), cost2, 1.0);
+        float cosp8 = fma(fma(fma(fma(128.0, cosp2, -256.0), cosp2, 160.0), cosp2, -32.0), cosp2, 1.0);
+        float sint8 = fma(fma(fma(128.0, cost2, -192.0), cost2, 80.0), cost2, -8.0) * sint * cost;
+        float sinp8 = fma(fma(fma(128.0, cosp2, -192.0), cosp2, 80.0), cosp2, -8.0) * sinp * cosp;
+        z = r * vec3(sint8 * sinp8, cosp8, cost8 * sinp8);
         z += p;
-        */
-        float theta = 8.0 * atan(z.x, z.z);
-        float phi = 8.0 * acos(z.y / r);
-        z = pow(r, 8.0) * vec3(sin(theta) * sin(phi), cos(phi), cos(theta) * sin(phi)) + p;
-        //z = vec3(z.x * z.x - z.y * z.y - z.z * z.z, 2.0 * z.y * (z.x), 2.0 * z.x * z.z);
-        //z = vec3(z.x * z.x - z.y * z.y - z.z * z.z, 2.0 * z.y * (z.x + z.z), 2.0 * z.x * z.z);
-        //z = vec3(z.x * z.x - z.y * z.y - z.z * z.z, 2.0 * z.y * (z.x + z.z), 2.0 * z.x * z.z);
-        //z += p;
+
+        // f_n(c) = f_n-1(c)^8 + c
+        // Using Inverse Trigonometric Functions
+        // For Some Reason acos Is Much Faster Than atan For Me
+        //float theta = 8.0 * acos(z.z * invl);
+        //float phi = 8.0 * acos(z.y * invr);
+        //z = r * vec3(sin(theta) * sin(phi), cos(phi), cos(theta) * sin(phi)) + p;
         r = length(z);
 
         // This Is Placed At Bottom Otherwise We Waste The Last Iteration Calculation
